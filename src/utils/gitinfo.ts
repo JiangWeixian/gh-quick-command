cdd6de3a618196a3931f630ac45f2e63cc6a3598
JW <jiangweixian1994@gmail.com>
2020-08-04 10:32:26 +0800

    fix open org repo (#3)

--- src/utils/gitinfo.ts
+++ src/utils/gitinfo.ts
@@ -1,10 +1,12 @@
 import * as getGithubUsername from 'git-user-name'
 import * as getRepoInfo from 'git-repo-info'
 import * as getRepoName from 'git-repo-name'
+const gitRemoteOriginUrl = require('git-remote-origin-url')
 
 let username: string | null = ''
 let info: ReturnType<typeof getRepoInfo> = {} as ReturnType<typeof getRepoInfo>
 let reponame: string = ''
+let url = ''
 
 export const getGitInfo = () => {
   try {
@@ -28,4 +30,16 @@ export const getUsename = () => {
     // ignore and donothing
   }
   return username
+}
+
+export const getRepoUrl = async () => {
+  const regex = /git@github\.com:([\w\/-]+)\.git/
+  try {
+    url = await gitRemoteOriginUrl()
+    const result = url.match(regex)
+    url = result && result[1] || ''
+  } catch (e) {
+    // do nothing
+  }
+  return url
 }

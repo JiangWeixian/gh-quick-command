e438ae24c012f6333b8e30833e481083dae4eda5
JW <jiangweixian1994@gmail.com>
2020-08-05 13:34:50 +0800

    refractor (#5)

--- src/ghp-my.tsx
+++ src/ghp-my.tsx
@@ -18,25 +18,27 @@ program.usage('my [stars | repos]')
 process.on('exit', () => {})
 
 program.parse(process.argv)
-let tabname = program.args && program.args[0]
-const maps = {
-  'stars': `${ORIGIN.home}/${getUsename()}?tab=stars`,
-  'repos': `${ORIGIN.home}/${getUsename()}?tab=repositories`,
+type Commands = 'stars' | 'repos'
+let type = program.args && program.args[0] as Commands
+
+
+const commander = (type: Commands) => {
+  const name = getUsename()
+  switch (type) {
+    case 'stars':
+      return `${ORIGIN.home}/${name}?tab=stars`
+    case 'repos':
+      return `${ORIGIN.home}/${getUsename()}?tab=repositories`
+    default:
+      return `${ORIGIN.home}/${getUsename()}?tab=repositories`
+  }
 }
 
 const run = () => {
-  if (!getUsename()) {
-    logger.fatal(`🐈 github username not found!`)
-  }
-  if (!tabname || !maps[tabname]) {
-    logger.fatal(`🐈 wrooong input!`)
-  }
-  if (!getUsename() || !maps[tabname]) {
-    process.exit(1)
-  }
-  exec(`open ${maps[tabname]}`, (err) => {
+  const command = commander(type)
+  exec(`open ${command}`, (err) => {
     if (err) {
-      logger.fatal(`failed open ${maps[tabname]}`)
+      logger.fatal(`failed open ${command}`)
     }
   })
 }

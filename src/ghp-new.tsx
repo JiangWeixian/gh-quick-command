cc99486876ac27da4fbfdc1c59e4da1bd513aefe
jiangWeixian <jiangweixian1994@gmail.com>
2020-08-05 13:21:22 +0800

    0.4.2

--- src/ghp-new.tsx
+++ src/ghp-new.tsx
@@ -21,21 +21,22 @@ program.parse(process.argv)
 type Commands = 'repo' | 'token' | 'pr'
 let type = program.args && program.args[0] as Commands
 
-const commander = (type: Commands) => {
+const commander = async (type: Commands) => {
   switch (type) {
     case 'repo':
       return `${ORIGIN.home}/new`
     case 'token':
       return `${ORIGIN.setting}/tokens/new`
     case 'pr':
-      return `${ORIGIN.home}/${getRepoUrl()}/compare/${getGitInfo().info.branch}?expand=1`
+      const repoUrl = await getRepoUrl()
+      return `${ORIGIN.home}/${repoUrl}/compare/${getGitInfo().info.branch}?expand=1`
     default:
       return `${ORIGIN.home}/new`
   }
 }
 
-const run = () => {
-  const command = commander(type)
+const run = async () => {
+  const command = await commander(type)
   exec(`open ${command}`, (err) => {
     if (err) {
       logger.fatal(`failed open ${command}`)

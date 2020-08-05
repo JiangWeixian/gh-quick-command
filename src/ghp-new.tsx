05d15c93e7091133506d922338ce854a8db06d3c
jiangWeixian <jiangweixian1994@gmail.com>
2020-08-05 13:11:25 +0800

    ♻️ (new): use common

--- src/ghp-new.tsx
+++ src/ghp-new.tsx
@@ -3,7 +3,7 @@ import { exec } from 'child_process'
 
 import logger from './utils/logger'
 import { ORIGIN } from './utils/constants'
-import { getGitInfo } from './utils/gitinfo'
+import { getGitInfo, getRepoUrl } from './utils/gitinfo'
 
 /**
  * Usage.
@@ -18,29 +18,27 @@ program.usage('new')
 process.on('exit', () => {})
 
 program.parse(process.argv)
-let typename = program.args && program.args[0]
-const maps = {
-  'repo': `${ORIGIN.home}/new`,
-  'token': `${ORIGIN.setting}/tokens/new`,
-  'pr': `${ORIGIN.home}/${getGitInfo().username}/${getGitInfo().reponame}/compare/${getGitInfo().info.branch}?expand=1`
+type Commands = 'repo' | 'token' | 'pr'
+let type = program.args && program.args[0] as Commands
+
+const commander = (type: Commands) => {
+  switch (type) {
+    case 'repo':
+      return `${ORIGIN.home}/new`
+    case 'token':
+      return `${ORIGIN.setting}/tokens/new`
+    case 'pr':
+      return `${ORIGIN.home}/${getRepoUrl()}/compare/${getGitInfo().info.branch}?expand=1`
+    default:
+      return `${ORIGIN.home}/new`
+  }
 }
 
 const run = () => {
-  if (!typename) {
-    exec(`open ${maps.repo}`, (err) => {
-      if (err) {
-        logger.fatal(`failed open ${maps.repo}`)
-      }
-    })
-    process.exit(1)
-  }
-  if (!maps[typename]) {
-    logger.fatal(`🐈 wrooong input!`)
-    process.exit(1)
-  }
-  exec(`open ${maps[typename]}`, (err) => {
+  const command = commander(type)
+  exec(`open ${command}`, (err) => {
     if (err) {
-      logger.fatal(`failed open ${maps[typename]}`)
+      logger.fatal(`failed open ${command}`)
     }
   })
 }
